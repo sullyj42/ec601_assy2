7642689ee8ed9a098dccb60d554f01bddf2abb44
Jeremiah Sullivan <sully42@gmail.com>
2019-09-23 18:03:28 -0400

    Running testscript

--- testscript.sh
+++ testscript.sh
@@ -1,3 +1,9 @@
-./splitargs larg erwaf relg rgae awerger ergwertg ret rgwtg wtghwtrhrwth  twh wrth 1> splitargsOUT/stdout.txt 2> splitargsOUT/stderr.txt
 
 
+g++ -o splitargs splitargs.cpp; 
+
+./splitargs good one two three four five ret rgwtg wtghwtrhrwth  twh wrth 1> splitargsOUT/stdout.txt 2> splitargsOUT/stderr.txt; 
+
+g++ -o numtest test_numbers.cpp; 
+
+./numtest; 
